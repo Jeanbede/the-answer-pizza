7bfbf9ca38963bd7c2789287478c6f782403b2b3
Jeanbede <njeanbeda@gmail.com>
2021-01-15 02:11:32 +0300

    adding commands o my webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,4 @@
-// var blink = document.getElementById('blink');
-// setInterval(function() {
-//     blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
-// }, 1500);
-
-
-
-// $(document).ready(function () {
-//     $('#design').click(function () {
-//         $(this).hide();
-//         $('#design-p').show();
-//     });
-  
-//         $('#design-p').click(function(){
-//              $(this).hide();
-//             $('#design').show();
-//     });
-                    
-//              $('#development').click(function () {
-//             $(this).toggle();
-//             $('#development-p').toggle();
-//          });
-
-//          $('#dvelopment-p').click(function () {
-//             $(this).toggle();
-//             $('development').toggle();
-//          });
-
-//          $('#product').click(function () {
-//             $(this).toggle();
-//             $('#product-p').toggle();
-//          });
-
-//          $('#product-p').click(function () {
-//             $(this).toggle();
-//             $('product').toggle();
-//          });
-
-
-
+// pop message 
 
          $(".container").submit(function(event) {
             event.preventDefault();
@@ -46,117 +7,53 @@
             document.getElementById("my-form").reset();
         });
    
+        // blinking code
 
     var blink = document.getElementById('blink');
 setInterval(function() {
     blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
 }, 100);
 
-                         
-             
-        var btn = document.getElementById("myBtn");
-
-           btn.onclick = function(event) {
-            
-            var x = document.forms["myForm"]["naame"].value;
-            if (x == "") {
-                alert("Name not entered");
-                return false;
-            }
-            var y = document.forms["myForm"]["naame"].value;
-            if (y == "") {
-                alert("Second name not entered");
-                return false;
-            }
-            var z = document.forms["myForm"]["naame"].value;
-            if (z == "") {
-                alert("You have no message to send");
-                return false;
-            }
-            var person1Input = $("input#form-name").val();
-            $("form-contact").append(person1Input);
-        
-            $('myBtn').push('show');
-            event.preventDefault();
-        };
-                          
- 
-//     $("#my-form").submit(function(event) {
-//       event.preventDefault();
-//       var name = $("#contact-name").val();
-//       alert("Our Dear Client," + name + ".We have received your message,Thank you for reaching out to us!!!");
-//       document.getElementById("my-form").reset();
-//   });
-
-     
+    //    Hover code  
 
   $(document).ready(function(){
-    $("#design").click(function(){
-        $("#design-p").toggle()
-        $("#design").toggle()
+    $("#design").hover(function(){
+        $("#design-p").show()
+        $("#design").hive()
     });
 });
 $(document).ready(function(){
-    $("#design-p").click(function(){
-        $("#design").toggle()
-        $("#design-p").toggle()
+    $("#design-p").hover(function(){
+        $("#design").show()
+        $("#design-p").hide()
     });
 });
 $(document).ready(function(){
-    $("#development").click(function(){
-        $("#development-p").toggle()
-    $("#development").toggle()
+    $("#development").hover(function(){
+        $("#development-p").show()
+    $("#development").hide()
     });
 });
 $(document).ready(function(){
-    $("#development-p").click(function(){
-        $("#development").toggle()
-        $("#development-p").toggle()
+    $("#development-p").hover(function(){
+        $("#development").show()
+        $("#development-p").hide()
     });
 });
 $(document).ready(function(){
-    $("#product").click(function(){
+    $("#product").hover(function(){
         $("#product-p").show()
         $("#product").hide()
     });
 });
 $(document).ready(function(){
-    $("#product-p").click(function(){
+    $("#product-p").hover(function(){
         $("#product").show()
         $("#product-p").hide()
     });
 });
 
-// $(document).ready(function(){
-//     $("#wrap1").hover(function(){
-//         $("#hide-1").toggle();
-//     })
-//     $("#wrap2").hover(function(){
-//         $("#hide-2").toggle();
-//     })
-//     $("#wrap3").hover(function(){
-//         $("#hide-3").toggle();
-//     })
-//     $("#wrap4").hover(function(){
-//         $("#hide-4").toggle();
-//     })
-//     $("#wrap5").hover(function(){
-//         $("#hide-5").toggle();
-//     })
-//     $("#wrap6").hover(function(){
-//         $("#hide-6").toggle();
-//     })
-//     $("#wrap7").hover(function(){
-//         $("#hide-7").toggle();
-//     })
-//     $("#wrap8").hover(function(){
-//         $("#hide-8").toggle();
-//     })
-// });
-
-
-
-
+// Executive js code
 var contacts = [];
 
 function addcontact() {
@@ -189,18 +86,7 @@ function listContacts() {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Calculator code
 
 function updatesum() {
     document.input.total.value = (document.input.time.value -0) + (document.input.cost.value -0);
@@ -208,11 +94,6 @@ function updatesum() {
     }
 
 
-
-
-
-
-       
 
 
 
